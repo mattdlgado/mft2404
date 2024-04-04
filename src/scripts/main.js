bd4d2df39c8a43b81ade94f558aa577574318a85
mattdlgado <lardeastudio@gmail.com>
2024-04-04 18:05:55 +0200

    Change ponentes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,7 +4,7 @@ import { Navigation } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/navigation";
 
-var swiper = new Swiper(".ponentes", {
+/*var swiper = new Swiper(".ponentes", {
   modules: [Navigation],
   slidesPerView: 1,
   spaceBetween: 8,
@@ -26,7 +26,7 @@ var swiper = new Swiper(".ponentes", {
     nextEl: ".next-ponente",
     prevEl: ".prev-ponente",
   },
-});
+});*/
 
 // GSAP
 import { gsap } from "gsap";
@@ -39,10 +39,28 @@ const iconsTimeline = gsap.timeline({
     start: "top bottom",
     end: "top center",
     scrub: true,
+    markers: true
   },
 });
 gsap.set(".swiper-slide", { opacity: 1, y: 0 });
 iconsTimeline.from(".swiper-slide", {
+  duration: 0.5,
+  ease: "power4.out",
+  stagger: 0.1,
+  opacity: 0,
+  y: 80,
+});
+
+const iconsTimeline2 = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".ponentes",
+    start: "top bottom",
+    end: "top center",
+    scrub: true,
+  },
+});
+gsap.set(".ponente", { opacity: 1, y: 0 });
+iconsTimeline2.from(".ponente", {
   duration: 0.5,
   ease: "power4.out",
   stagger: 0.1,
