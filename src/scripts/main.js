baa6f5287b5198c70450c6de730d2ae9b427f435
mattdlgado <lardeastudio@gmail.com>
2024-04-11 09:58:04 +0200

    Update business section

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -12,24 +12,6 @@ var swiper = new Swiper(".images", {
   },
 });
 
-// GSAP
-import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-gsap.registerPlugin(ScrollTrigger);
-
-document.querySelectorAll(".business > div").forEach((item) => {
-  gsap.from(item, {
-    scrollTrigger: {
-      trigger: item,
-      start: "top bottom",
-      toggleActions: "play none none reverse",
-    },
-    y: 100,
-    duration: 2,
-    ease: "power4.out",
-  });
-});
-
 // Lenis
 import Lenis from "@studio-freight/lenis";
 const lenis = new Lenis();
